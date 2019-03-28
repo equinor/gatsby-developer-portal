38be64a4d028be4c985d6c0f9a37e76786872c90
cbuv <cbuv@equinor.com>
2019-03-28 10:49:30 +0100

    add fulltext search in search page
    Signed-off-by: cbuv <cbuv@equinor.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,10 @@ module.exports = {
                 name: "Careers",
                 url: "https://www.equinor.com/en/careers/job-vacancies.html"
             },
+            {
+                name: "Search",
+                url: "/search"
+            }
         ]
     },
     plugins: [
@@ -108,6 +112,46 @@ module.exports = {
                 matomoUrl: 'https://matomo.sdpaks.equinor.com',
                 siteUrl: 'https://developer.equinor.com'
             }
-        }
+        },
+        {
+            resolve: `gatsby-plugin-lunr`,
+            options: {
+                languages: [
+                    {
+                        // ISO 639-1 language codes. See https://lunrjs.com/guides/language_support.html for details
+                        name: 'en',
+                        // A function for filtering nodes. () => true by default
+                        filterNodes: (node) => {
+                          if (node.frontmatter) {
+                            return node.frontmatter;
+                          }
+                          return false;
+                        },
+                        // Add to index custom entries, that are not actually extracted from gatsby nodes
+                        // customEntries: [{ title: 'Pictures', content: 'awesome pictures', url: '/pictures' }],
+                    },
+                ],
+                // Fields to index. If store === true value will be stored in index file.
+                // Attributes for custom indexing logic. See https://lunrjs.com/docs/lunr.Builder.html for details
+                fields: [
+                    { name: 'title', store: true, attributes: { boost: 20}},
+                    { name: 'content', store: true },
+                    { name: 'collection', store: true },
+                    { name: 'url', store: true },
+                ],
+                // How to resolve each field's value for a supported node type
+                resolvers: {
+                    // For any node of type MarkdownRemark, list how to resolve the fields' values
+                    MarkdownRemark: {
+                        title: node => node.frontmatter.title,
+                        content: node => node.rawMarkdownBody,
+                        collection: node => node.frontmatter.collection,
+                        url: node => node.fields.url,
+                    },
+                },
+                //custom index file name, default is search_index.json
+                filename: 'search_index.json',
+            },
+        },
     ],
 };
