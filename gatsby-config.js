4d580928c78d64d4eebddd5ff73f8539e6b2e716
cbuv <cbuv@equinor.com>
2019-05-09 09:54:21 +0200

    prettier on root *.js
    Signed-off-by: cbuv <cbuv@equinor.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,156 +1,156 @@
 module.exports = {
-    siteMetadata: {
-        title: `Title`,
-        subTitle: `Sub title`,
-        author: `Equinor ASA`,
-        description: `Site for developers`,
-        siteUrl: `https://developer.equinor.com`,
-        social: {
-            twitter: `equinorasa`,
-        },
-        menuLinks: [
-            {
-                name: "Toolbox",
-                link: "/toolbox"
-            },
-            {
-                name: "Blog",
-                link: "/blog"
-            },
-            {
-                name: "Github",
-                url: "https://github.com/orgs/equinor"
-            },
-            {
-                name: "Careers",
-                url: "https://www.equinor.com/en/careers/job-vacancies.html"
-            },
-            {
-                name: "Search",
-                url: "/search"
-            }
-        ]
+  siteMetadata: {
+    title: `Title`,
+    subTitle: `Sub title`,
+    author: `Equinor ASA`,
+    description: `Site for developers`,
+    siteUrl: `https://developer.equinor.com`,
+    social: {
+      twitter: `equinorasa`,
     },
-    plugins: [
-        `gatsby-plugin-styled-components`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/content/blog`,
-                name: `blog`,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/content/docs`,
-                name: `docs`,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/content/assets/`,
-                name: `images`,
-            },
-        },
-        {
-            resolve: "gatsby-plugin-react-svg",
-            options: {
-                path: `${__dirname}/src/assets/icons/`,
-                name: 'icons'
-            }
-        },
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [
-                    {
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 590,
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-responsive-iframe`,
-                        options: {
-                            wrapperStyle: `margin-bottom: 1.0725rem`,
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-prismjs`,
-                        options: {
-                            showLineNumbers: true
-                        }
-                    },
-                    `gatsby-remark-copy-linked-files`,
-                    `gatsby-remark-smartypants`,
-                ],
-            },
-        },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        `gatsby-plugin-feed`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `developer.equinor.com`,
-                short_name: `Developer`,
-                start_url: `/`,
-                background_color: `#ffffff`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: `content/assets/gatsby-icon.png`,
-            },
-        },
-        `gatsby-plugin-offline`,
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: 'gatsby-plugin-matomo',
-            options: {
-                siteId: '2',
-                matomoUrl: 'https://matomo.sdpaks.equinor.com',
-                siteUrl: 'https://developer.equinor.com',
-                disableCookies: true
-            }
-        },
-        {
-            resolve: `gatsby-plugin-lunr`,
-            options: {
-                languages: [
-                    {
-                        // ISO 639-1 language codes. See https://lunrjs.com/guides/language_support.html for details
-                        name: 'en',
-                        // A function for filtering nodes. () => true by default
-                        filterNodes: (node) => {
-                          if (node.frontmatter) {
-                            return node.frontmatter;
-                          }
-                          return false;
-                        },
-                        // Add to index custom entries, that are not actually extracted from gatsby nodes
-                        // customEntries: [{ title: 'Pictures', content: 'awesome pictures', url: '/pictures' }],
-                    },
-                ],
-                // Fields to index. If store === true value will be stored in index file.
-                // Attributes for custom indexing logic. See https://lunrjs.com/docs/lunr.Builder.html for details
-                fields: [
-                    { name: 'title', store: true, attributes: { boost: 20}},
-                    { name: 'content', store: true },
-                    { name: 'url', store: true },
-                ],
-                // How to resolve each field's value for a supported node type
-                resolvers: {
-                    // For any node of type MarkdownRemark, list how to resolve the fields' values
-                    MarkdownRemark: {
-                        title: node => node.frontmatter.title,
-                        content: node => node.rawMarkdownBody,
-                        url: node => node.fields.url,
-                    },
-                },
-                //custom index file name, default is search_index.json
-                filename: 'search_index.json',
-            },
-        },
+    menuLinks: [
+      {
+        name: "Toolbox",
+        link: "/toolbox",
+      },
+      {
+        name: "Blog",
+        link: "/blog",
+      },
+      {
+        name: "Github",
+        url: "https://github.com/orgs/equinor",
+      },
+      {
+        name: "Careers",
+        url: "https://www.equinor.com/en/careers/job-vacancies.html",
+      },
+      {
+        name: "Search",
+        url: "/search",
+      },
     ],
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/docs`,
+        name: `docs`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets/`,
+        name: `images`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        path: `${__dirname}/src/assets/icons/`,
+        name: "icons",
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              showLineNumbers: true,
+            },
+          },
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `developer.equinor.com`,
+        short_name: `Developer`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: "gatsby-plugin-matomo",
+      options: {
+        siteId: "2",
+        matomoUrl: "https://matomo.sdpaks.equinor.com",
+        siteUrl: "https://developer.equinor.com",
+        disableCookies: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-lunr`,
+      options: {
+        languages: [
+          {
+            // ISO 639-1 language codes. See https://lunrjs.com/guides/language_support.html for details
+            name: "en",
+            // A function for filtering nodes. () => true by default
+            filterNodes: node => {
+              if (node.frontmatter) {
+                return node.frontmatter;
+              }
+              return false;
+            },
+            // Add to index custom entries, that are not actually extracted from gatsby nodes
+            // customEntries: [{ title: 'Pictures', content: 'awesome pictures', url: '/pictures' }],
+          },
+        ],
+        // Fields to index. If store === true value will be stored in index file.
+        // Attributes for custom indexing logic. See https://lunrjs.com/docs/lunr.Builder.html for details
+        fields: [
+          { name: "title", store: true, attributes: { boost: 20 } },
+          { name: "content", store: true },
+          { name: "url", store: true },
+        ],
+        // How to resolve each field's value for a supported node type
+        resolvers: {
+          // For any node of type MarkdownRemark, list how to resolve the fields' values
+          MarkdownRemark: {
+            title: node => node.frontmatter.title,
+            content: node => node.rawMarkdownBody,
+            url: node => node.fields.url,
+          },
+        },
+        //custom index file name, default is search_index.json
+        filename: "search_index.json",
+      },
+    },
+  ],
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,176 +1,173 @@
 const path = require(`path`);
-const {createFilePath} = require(`gatsby-source-filesystem`);
+const { createFilePath } = require(`gatsby-source-filesystem`);
 const _ = require("lodash");
 const fetch = require("node-fetch");
-const fetchBase64 = require('fetch-base64');
+const fetchBase64 = require("fetch-base64");
 
 require("dotenv").config({
-  path: '.env',
+  path: ".env",
 });
 
 // Context: will be passed in to the page query as graphql variables.
 
-exports.createPages = async ({graphql, actions}) => {
-    const {
-        createPage
-    } = actions;
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
 
-    const blogTemplate = path.resolve(`./src/templates/blog-template.js`);
-    const tagTemplate = path.resolve("src/templates/tag-template.js");
-    const docTemplate = path.resolve("src/templates/doc-template.js");
+  const blogTemplate = path.resolve(`./src/templates/blog-template.js`);
+  const tagTemplate = path.resolve("src/templates/tag-template.js");
+  const docTemplate = path.resolve("src/templates/doc-template.js");
 
-    const result = await graphql(`
-      {
-        allMarkdownRemark(
-          filter: { fields: { collection: { eq: "blog" } } } 
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-                collection
-              }
-              frontmatter {
-                title
-                tags
-              }
+  const result = await graphql(`
+    {
+      allMarkdownRemark(
+        filter: { fields: { collection: { eq: "blog" } } }
+        sort: { fields: [frontmatter___date], order: DESC }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+              collection
+            }
+            frontmatter {
+              title
+              tags
             }
           }
         }
       }
-    `);
-
-    if (result.errors) {
-        console.log(result.errors);
-        throw new Error('Things broke, see console output above');
     }
+  `);
 
-    const posts = result.data.allMarkdownRemark.edges;
-    posts.forEach((post, index) => {
-        const previous = index === posts.length - 1 ? null : posts[index + 1].node;
-        const next = index === 0 ? null : posts[index - 1].node;
-        createPage({
-            path: `${post.node.fields.collection}${post.node.fields.slug}`,
-            component: blogTemplate,
-            context: {
-                slug: post.node.fields.slug,
-                previous,
-                next,
-            },
-        });
+  if (result.errors) {
+    console.log(result.errors);
+    throw new Error("Things broke, see console output above");
+  }
+
+  const posts = result.data.allMarkdownRemark.edges;
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node;
+    const next = index === 0 ? null : posts[index - 1].node;
+    createPage({
+      path: `${post.node.fields.collection}${post.node.fields.slug}`,
+      component: blogTemplate,
+      context: {
+        slug: post.node.fields.slug,
+        previous,
+        next,
+      },
     });
+  });
 
-    const resultDocs = await graphql(`
-      {
-        allMarkdownRemark(
-          filter: { fields: { collection: { eq: "docs" } } }
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-                collection
-              }
-              frontmatter {
-                title
-                tags
-              }
+  const resultDocs = await graphql(`
+    {
+      allMarkdownRemark(
+        filter: { fields: { collection: { eq: "docs" } } }
+        sort: { fields: [frontmatter___date], order: DESC }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+              collection
+            }
+            frontmatter {
+              title
+              tags
             }
           }
         }
       }
-    `);
-
-    if (resultDocs.errors) {
-        console.log(resultDocs.errors);
-        throw new Error('Things broke, see console output above');
     }
+  `);
 
-    const docs = resultDocs.data.allMarkdownRemark.edges;
-    docs.forEach((doc) => {
-        createPage({
-            path: `${doc.node.fields.collection}${doc.node.fields.slug}`,
-            component: docTemplate,
-            context: {
-                slug: doc.node.fields.slug
-            },
-        });
-    });
+  if (resultDocs.errors) {
+    console.log(resultDocs.errors);
+    throw new Error("Things broke, see console output above");
+  }
 
-    let tags = [];
-    _.each(posts, edge => {
-        if (_.get(edge, "node.frontmatter.tags")) {
-            tags = tags.concat(edge.node.frontmatter.tags)
-        }
+  const docs = resultDocs.data.allMarkdownRemark.edges;
+  docs.forEach(doc => {
+    createPage({
+      path: `${doc.node.fields.collection}${doc.node.fields.slug}`,
+      component: docTemplate,
+      context: {
+        slug: doc.node.fields.slug,
+      },
     });
-    _.each(docs, edge => {
-        if (_.get(edge, "node.frontmatter.tags")) {
-            tags = tags.concat(edge.node.frontmatter.tags)
-        }
+  });
+
+  let tags = [];
+  _.each(posts, edge => {
+    if (_.get(edge, "node.frontmatter.tags")) {
+      tags = tags.concat(edge.node.frontmatter.tags);
+    }
+  });
+  _.each(docs, edge => {
+    if (_.get(edge, "node.frontmatter.tags")) {
+      tags = tags.concat(edge.node.frontmatter.tags);
+    }
+  });
+  // Eliminate duplicate tags
+  tags = _.uniq(tags);
+  tags.forEach(tag => {
+    createPage({
+      path: `/tags/${_.kebabCase(tag)}/`,
+      component: tagTemplate,
+      context: {
+        tag,
+      },
     });
-    // Eliminate duplicate tags
-    tags = _.uniq(tags);
-    tags.forEach(tag => {
-        createPage({
-            path: `/tags/${_.kebabCase(tag)}/`,
-            component: tagTemplate,
-            context: {
-                tag,
-            },
-        })
-    });
+  });
 };
 
-exports.onCreateNode = async ({node, actions, getNode}) => {
-    const {
-        createNodeField
-    } = actions;
+exports.onCreateNode = async ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
 
-    if (node.internal.type === `MarkdownRemark`) {
-        const value = createFilePath({node, getNode});
-        createNodeField({
-            name: `slug`,
-            node,
-            value,
-        })
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode });
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    });
+  }
+
+  if (node.internal.type === `MarkdownRemark`) {
+    createNodeField({
+      name: `collection`,
+      node,
+      value: getNode(node.parent).sourceInstanceName,
+    });
+  }
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const authors = node.frontmatter.authors;
+    const config = {
+      headers: {
+        Authorization: `token ${process.env.GITHUB_PERSONAL_TOKEN}`,
+      },
+    };
+    const data = [];
+    const getProfile = author =>
+      fetch("https://api.github.com/users/" + author, config);
+    for (let i = 0; i < authors.length; i++) {
+      const response = await getProfile(authors[i]);
+      const json = await response.json();
+      const image = await fetchBase64.remote(json.avatar_url);
+      console.log("downloaded avatar", authors[i]);
+      data.push({
+        image: image[1],
+        name: json.name,
+      });
     }
 
-    if (node.internal.type === `MarkdownRemark`) {
-        createNodeField({
-            name: `collection`,
-            node,
-            value: getNode(node.parent).sourceInstanceName
-        })
-    }
-
-    if (node.internal.type === `MarkdownRemark`) {
-      const authors = node.frontmatter.authors;
-      const config = {
-        headers: {
-          'Authorization': `token ${process.env.GITHUB_PERSONAL_TOKEN}`
-        }
-      };
-      const data = [];
-      const getProfile = (author) => fetch("https://api.github.com/users/" + author, config);
-      for (let i = 0; i < authors.length; i++) {
-        const response = await getProfile(authors[i]);
-        const json = await response.json();
-        const image = await fetchBase64.remote(json.avatar_url);
-        console.log('downloaded avatar', authors[i]);
-        data.push({
-          image: image[1],
-          name: json.name
-        });
-      }
-
-      createNodeField({
-        name: `authors`,
-        node,
-        value: data
-      })
-    }
+    createNodeField({
+      name: `authors`,
+      node,
+      value: data,
+    });
+  }
 };
