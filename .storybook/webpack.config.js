6be4d32ad95701ee479fabf808af12b3e0a5c17b
cbuv <cbuv@equinor.com>
2019-05-22 11:47:31 +0200

    add gatsby config
    Signed-off-by: cbuv <cbuv@equinor.com>

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,23 @@
-// you can use this file to add your custom webpack plugins, loaders and anything you like.
-// This is just the basic way to add additional webpack configurations.
-// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
-
-// IMPORTANT
-// When you add this file, we won't add the default configurations which is similar
-// to "React Create App". This only has babel loader to load JavaScript.
-
-module.exports = {
-  plugins: [
-    // your custom plugins
-  ],
-  module: {
-    rules: [
-      // add your custom rules.
-    ],
-  },
-};
+module.exports = ({ config }) => {
+  // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+  config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
+  
+  // use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7)
+  config.module.rules[0].use[0].loader = require.resolve("babel-loader")
+  
+  // use @babel/preset-react for JSX and env (instead of staged presets)
+  config.module.rules[0].use[0].options.presets = [
+    require.resolve("@babel/preset-react"),
+    require.resolve("@babel/preset-env"),
+  ]
+  
+  // use @babel/plugin-proposal-class-properties for class arrow functions
+  config.module.rules[0].use[0].options.plugins = [
+    require.resolve("@babel/plugin-proposal-class-properties"),
+  ]
+  
+  // Prefer Gatsby ES6 entrypoint (module) over commonjs (main) entrypoint
+  config.resolve.mainFields = ["browser", "module", "main"]
+  
+  return config
+}
